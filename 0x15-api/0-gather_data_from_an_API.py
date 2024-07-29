f1353b57735de31115665e3f537f0d3625eea026
Manyala1 <paulmuthama60@gmail.com>
2024-07-29 22:16:30 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,25 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-
 """
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+python script that, using a REST API, for a give employee ID, returns
+information about his/her TODO list progress
 """
 
-from requests import get
-from sys import argv
+import requests
+import sys
 
-
-if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-    completed = 0
-    total = 0
-    tasks = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
-
-    for i in data2:
-        if i.get('id') == int(argv[1]):
-            employee = i.get('name')
-
-    for i in data:
-        if i.get('userId') == int(argv[1]):
-            total += 1
-
-            if i.get('completed') is True:
-                completed += 1
-                tasks.append(i.get('title'))
-
-    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
-                                                          total))
-
-    for i in tasks:
-        print("\t {}".format(i))
+if __name == "__main__":
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    employee_id = sys.argv[1]
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    user = user_response.json()
+    params = {"userId": employee_id}
+    todos_response = requests.get(url + "todos", params=params)
+    todos = todos_response.json()
+    completed = []
+    for todo in todos:
+        if todo.get("completes") is True:
+            completed.append(todo.get("title"))
+    print("Employee {} is done with tass({}/)".format(user.get("name").
+                                                     len(completed).len(todos)))
+    for complete in completed:
+          print("\t {}".format(complete))
