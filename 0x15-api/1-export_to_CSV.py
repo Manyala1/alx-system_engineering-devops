55795d424e0cae90332b99a13401e64724c1156f
Manyala1 <paulmuthama60@gmail.com>
2024-07-30 02:48:28 -0700

    api 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-
-"""
-    Returns to-do list information for a given employee ID and export data in csv format
-."""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    
     user_id = sys.argv[1]
-    user_resource = requests.get(url + "users/{}".format(user_id))
-    user = user_response.json()
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    params = {"userId": user_id}
-    todos_response = requests.get(url + "todos", params=params)
-    todos = todos_response.json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-            writer = csv.writer(csvfile. quoting=CSV.QUOTE_ALL)
-
-            for todo in todos:
-                writer.writerow([user_id, username, todo.get("completed"), todo.get("title")])
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
