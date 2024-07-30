aff0335cfeeb7fa2402b1adb6b73d112b5a332af
Manyala1 <paulmuthama60@gmail.com>
2024-07-30 01:19:59 -0700

    api 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,23 +3,21 @@
 """
     Returns to-do list information for a given employee ID and export data in csv format
 ."""
+import csv
 import requests
 import sys
-import csv
 
-def fetch_employee_todo_progress(emloyee_id):
+def fetch_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(f"{base_url}/users/{employee_id}").json()
+    username = user.get('username')
 
-    employee = requests.get(f"{base_url}/users/{employee_id}").json()
-    employee_name = employee.get('username')
-
-    todos = request.get(f"{base_url}/todos", params={'userId': employee_id}).json()
-
-    with open(f"{employee_id}.csv", mode='w', newline='') as file:
-        writer = csv.write(file)
+    todos = requests.get(f"{base_url}/todos", params={'userId': employee_id}).json()
+    filename = f"{employee_id}.csv"
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
         writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
         for todo in todos:
-            writer.writerow([employee_id, employee_name, todo['completed'], todo['title']])
+            writer.writerow([employee_id, username, todo['completed'], todo['title']])
 
-    print(f"Data for employee {employee_name} (ID: {employee_id}) has been written to {employee_id}.csv")
+    print(f"Data for employee {username} (ID: {employee_id}) has been written to {filename}")
